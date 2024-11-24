496aedca0e0e902942ecdefea35eee96ce787a9f
carlhalbreiner <carl.halbreiner@gmail.com>
2024-11-24 09:27:36 -0800

    Design Updates
    Changed navbar to be more readable and clean in index.css, index.html
    
    Added local jquery js file so that it can be worked on offline.
    
    removed hover effects from subject containers in index.js

--- index.js
+++ index.js
@@ -19,27 +19,11 @@ $(document).ready(function() {
     });
 
     //mouse interaction for lighting subject
-    $(".subject1").mouseenter(function(){
-        $("#light_link").stop();
-        $("#light_link").animate({opacity:1.0}, "fast");
-    });
-    $(".subject1").mouseleave(function() {
-        $("#light_link").stop();
-        $("#light_link").animate({opacity:0.8}, "fast");
-    });
     $(".subject1").click(function() {
         window.location.assign("./src/lighting/lighting.html");
     });
 
     //mouse interaction for web dev subject
-    $(".subject2").mouseenter(function(){
-        $("#web_link").stop();
-        $("#web_link").animate({opacity:1}, "fast");
-    });
-    $(".subject2").mouseleave(function() {
-        $("#web_link").stop();
-        $("#web_link").animate({opacity:0.8}, "fast");
-    });
     $(".subject2").click(function() {
         window.location.assign("./src/webdev/webdev.html");
     });
